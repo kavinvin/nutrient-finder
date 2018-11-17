b773e17a26c5129f71909f5a1a6017533e28df60
Weeruhputt Supsohmboon <59070162@it.kmitl.ac.th>
2018-11-17 16:52:55 +0700

    History model

---
+++ app/src/main/java/io/itforge/nutrient/models/HeaderNutrimentItem.java
@@ -0,0 +1,32 @@
+package io.itforge.nutrient.models;
+
+import android.support.annotation.NonNull;
+
+import static io.itforge.nutrient.utils.Utils.bold;
+
+/**
+ * Group of NutrimentItem with bold values
+ */
+public class HeaderNutrimentItem extends NutrimentItem {
+    /**
+     * Header with bold values
+     * @param title
+     * @param value
+     * @param servingValue
+     * @param unit
+     */
+    public HeaderNutrimentItem(@NonNull CharSequence title, @NonNull CharSequence value,
+                               @NonNull CharSequence servingValue, @NonNull CharSequence unit,
+                               @NonNull CharSequence modifier) {
+        super(bold(title), bold(value), bold(servingValue), bold(unit), bold(modifier));
+    }
+
+
+    /**
+     * Header with only bold title
+     * @param title
+     */
+    public HeaderNutrimentItem(@NonNull CharSequence title) {
+        super(bold(title), "", "", "", "");
+    }
+}
