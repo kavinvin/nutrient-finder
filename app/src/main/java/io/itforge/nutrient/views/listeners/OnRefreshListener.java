f519c97591016ca8f3b3899268bbda582fb65dc4
kavinvin <kavinvin.vin@gmail.com>
2018-11-17 22:16:51 +0700

    Fix view doesn't update

--- app/src/main/java/io/itforge/nutrient/views/listeners/OnRefreshListener.java
+++ app/src/main/java/io/itforge/nutrient/views/listeners/OnRefreshListener.java
@@ -1,9 +1,5 @@
 package io.itforge.nutrient.views.listeners;
 
-/**
- * Created by Lobster on 19.04.18.
- */
-
 public interface OnRefreshListener {
     void onRefresh();
 }
--- app/src/main/java/io/itforge/nutrient/views/listeners/OnRefreshView.java
+++ app/src/main/java/io/itforge/nutrient/views/listeners/OnRefreshView.java
@@ -2,10 +2,6 @@ package io.itforge.nutrient.views.listeners;
 
 import io.itforge.nutrient.models.State;
 
-/**
- * Created by Lobster on 19.04.18.
- */
-
 public interface OnRefreshView {
 
     void refreshView(State state);
