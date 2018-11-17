c0777b25a68a1b291d422f83dcb68e4531cd58e0
kavinvin <kavinvin.vin@gmail.com>
2018-11-17 22:14:10 +0700

    Create view listener

---
+++ app/src/main/java/io/itforge/nutrient/views/listeners/OnRefreshView.java
@@ -0,0 +1,13 @@
+package io.itforge.nutrient.views.listeners;
+
+import io.itforge.nutrient.models.State;
+
+/**
+ * Created by Lobster on 19.04.18.
+ */
+
+public interface OnRefreshView {
+
+    void refreshView(State state);
+
+}
